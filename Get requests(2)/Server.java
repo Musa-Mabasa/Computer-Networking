cb1964c9b68cfef2c1f5b4bd001cbcdea4c49d7b
Musa-Mabasa <musa14mabasa@gmail.com>
2022-04-26 04:12:09 +0200

    Add HTML

--- Get requests(2)/Server.java
+++ Get requests(2)/Server.java
@@ -209,7 +209,7 @@ public class Server
                         
                     }
                     
-                    System.out.println("Form "+form[0]);
+                    
                     if(form != null)
                     {
                         if(form[0].equals("Add"))
@@ -303,11 +303,11 @@ public class Server
                                         out.println("</div>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<label>Phone number</label>");
-                                        out.println("<input type=\"text\" name=\"phone number\" id=\"input\" placeholder=\"Enter surname\"");
+                                        out.println("<input type=\"text\" name=\"phone number\" id=\"input\" placeholder=\"Enter number\"");
                                         out.println("</div>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<button type=\"submit\" name=\"login\" class=\"btn\">Submit</button>");
-                                        out.println("</div>");
+                                        out.println("</div><br>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<button type=\"submit\" name=\"menu\" class=\"btn\">Menu</button>");
                                         out.println("</div>");
@@ -377,6 +377,193 @@ public class Server
                                                         System.out.println(e);
                                                     }
                                                     String [] decide = form2[3].split("\\=");
+
+                                                    System.out.println("Menu "+decide[0]);
+
+
+                                                    if(decide[0].equals("menu"))
+                                                    {
+                                                        val = false;
+                                                        PrintWriter out2 = new PrintWriter(socket.getOutputStream());
+
+                                                        out2.flush();
+                                                        // out2.close();
+                                                    }
+                                                
+
+
+                                                }
+                                            }
+                                            
+                                        }
+                                        
+                                    }
+                                    catch(IOException i)
+                                    {
+                                        System.out.println(i);
+                                    }
+                                    System.out.println("Musaaa: "+Boolean.toString(val));
+                                }
+                                
+                            }
+                            else if(form[0].equals("view"))
+                            {
+                                Boolean addStatus = null;
+                                while(!server.isClosed() && val)
+                                {
+                                    System.out.println("In view");
+                                    // socket = server.accept();
+                                    try
+                                    {
+                                        System.out.println("whyyyyyyy");
+                                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+                                        out.println("HTTP/1.1 200 OK");
+                                        out.println("Content-Type: text/html");
+                                        out.println("<!DOCTYPE html>");
+                                        out.println("<html>");
+                                        out.println("<head>");
+                                        out.println("<title>COntactList</title>");
+                                        out.println("<link rel=\"stylesheet\" href=\"index.css\">");
+                                        out.println("</head>");
+                                        out.println("\r\n");
+                                        out.println("<body>");
+                                        out.println("<style>");
+                                        out.println("body{");
+                                        out.println("background: url(\"https://png.pngtree.com/thumb_back/fw800/background/20190223/ourmid/pngtree-smart-tech-blue-phone-background-backgroundmobile-phonenew-erauniverseintelligentstereoscopicilluminatetechnologydynamicgridbackgroundadvertising-image_72371.jpg\") no-repeat center center fixed;");
+                                        out.println("-webkit-background-size: cover;");
+                                        out.println("-moz-background-size: cover;");
+                                        out.println("-o-background-size: cover;");
+                                        out.println("background-size: cover;");
+                                        out.println("background-color: green");
+                                        out.println(" width: 100%;");
+                                        out.println(" height: 100%;");
+                                        out.println("}");
+                                        out.println("form{");
+                                        out.println("margin-left:150px");
+                                        out.println("}");
+                                        out.println(".inputer{");
+                                        out.println("position: absolute;");
+                                        out.println("width: 484px;");
+                                        out.println("background-color: black;");
+                                        out.println("padding: 5%;");
+                                        out.println("margin-left: 28%;");
+                                        out.println("margin-top: 10%;");
+                                        out.println("opacity: 0.8;");
+                                        out.println("border-radius: 2%;");
+                                        out.println("}");
+                                        out.println("#input{");
+                                        out.println("margin-top: 1%;");
+                                        out.println("margin-bottom: 1%;");
+                                        out.println("border: none;");
+                                        out.println("border-bottom: solid ;");
+                                        out.println("background: transparent;");
+                                        out.println("margin-left: 1%;");
+                                        out.println("width: 90%;");
+                                        out.println("color: white;");
+                                        out.println("}");
+                                        out.println("label{");
+                                        out.println("color: white;");
+                                        out.println("}");
+                                        out.println("table{");
+                                        out.println("border-style:none;");
+                                        out.println("width : 100%");
+                                        out.println("}");
+                                        out.println("table th{");
+                                        out.println("color: white;");
+                                        out.println("margin-left:0%;");
+                                        out.println("}");
+                                        out.println("table td{");
+                                        out.println("color: white;");
+                                        out.println("}");
+                                        out.println(".path{");
+                                        out.println("align-items: center;");
+                                        out.println("position: relative;");
+                                        out.println("color: blue;");
+                                        out.println("font-size: small;");
+                                        out.println("margin-left: 30%;");
+                                        out.println("}");
+                                        out.println("h1{");
+                                        out.println("color: white;");
+                                        out.println("margin-left: 42%;");
+                                        out.println("}");
+                                        out.println("</style>");
+                                        out.println("<div class=\"inputer\">");
+                                        out.println("<table>");
+                                        out.println("<tr>");
+                                        out.println("<th>Name</th>");
+                                        out.println("<th>Surname</th>");
+                                        out.println("<th>Number</th>");
+                                        out.println("</tr>");
+                                        File infile = new File("Contacts.txt");
+                                        try(Scanner read = new Scanner(infile))
+                                        {
+                                            while(read.hasNextLine())
+                                            {
+                                                String line = read.nextLine();
+
+                                                String [] fields = line.split(",");
+
+                                                out.println("<tr>");
+                                                out.println("<td>"+ fields[0]+ "</th>");
+                                                out.println("<td>"+ fields[1] +"</th>");
+                                                out.println("<td>"+ fields[2] +"</th>");
+                                                out.println("</tr>");
+
+
+                                            }
+                                            
+                                        }
+                                        catch(IOException i)
+                                        {
+
+                                        }
+
+                                        out.println("</table><br>");
+                                        out.println("<form>");
+                                        out.println("<div class=\"input_group\">");
+                                        out.println("<button type=\"submit\" name=\"menu\" class=\"btn\">Menu</button>");
+                                        out.println("</div>");
+                                        out.println("</form>");
+                                        out.println("</div>");
+                                        out.flush();
+
+                                        out.close();
+
+                                        socket = server.accept();
+
+                                        in = new DataInputStream(
+                                            new BufferedInputStream(socket.getInputStream()));
+                            
+                                        firstIn = in.readLine();
+                            
+                                        System.out.println("Input2 "+firstIn);
+
+                                        String[] split2 = firstIn.split(" ");
+                                        
+                                        for(String sp : split)
+                                        {
+                                            System.out.println("First "+sp);
+                                        }
+                                        String[] form2 = null;
+                                        if(split.length > 1)
+                                        {
+                                            String[] que2 = split2[1].split("\\?");
+
+                                            for(String ques : que2)
+                                            {
+                                                System.out.println("Sec "+ques);
+                                            }
+                                            if(que2.length>1)
+                                            {
+                                                form2 = que2[1].split("\\&");
+                                                for(String fo : form2)
+                                                {
+                                                    System.out.println("Third "+fo);
+                                                }
+                                                if(form2.length>0)
+                                                {
+                                                    String [] decide = form2[0].split("\\=");
 
                                                     System.out.println("Menu "+decide[0]);
 
@@ -500,7 +687,7 @@ public class Server
                                         out.println("</div>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<button type=\"submit\" name=\"login\" class=\"btn\">Submit</button>");
-                                        out.println("</div>");
+                                        out.println("</div><br>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<button type=\"submit\" name=\"menu\" class=\"btn\">Menu</button>");
                                         out.println("</div>");
@@ -731,10 +918,7 @@ public class Server
                                         out.println("</div>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<button type=\"submit\" name=\"login\" class=\"btn\">Edit</button>");
-                                        out.println("</div>");
-                                        out.println("<div class=\"input_group\">");
-                                        out.println("<button type=\"submit\" name=\"menu\" class=\"btn\">Menu</button>");
-                                        out.println("</div>");
+                                        out.println("</div><br>");
                                         out.println("</form>");
                                         out.println("</div>");
 
@@ -961,7 +1145,7 @@ public class Server
                                         out.println("</div>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<label>Phone number</label>");
-                                        out.println("<input type=\"text\" name=\"phone number\" id=\"input\" placeholder=\"Enter surname\"");
+                                        out.println("<input type=\"text\" name=\"phone number\" id=\"input\" placeholder=\"Enter number\"");
                                         out.println("</div>");
                                         out.println("<div class=\"input_group\">");
                                         out.println("<button type=\"submit\" name=\"menu\" class=\"btn\">Submit</button>");
